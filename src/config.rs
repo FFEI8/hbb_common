f8ae5d10d2d9ab56358f4945f2a372a7e6946dc2
FFEI8 <157337852+FFEI8@users.noreply.github.com>
2025-02-07 10:27:43 +0800

    config.rs1

--- src/config.rs
+++ src/config.rs
@@ -97,8 +97,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["ru.bsjdhospitalfs.com"];
+pub const RS_PUB_KEY: &str = "JtI57duHG3hyJMsHqFzjI81acgbn7O6eH4952cilsw8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
